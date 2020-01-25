34a623d4214ea0572a32f979fd1427b188e85447
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-01-25 17:48:01 -0500

    use environment variables

--- endpoints.py
+++ endpoints.py
@@ -1,36 +1,18 @@
+import os
 from flask import Flask, json, g, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-token = "123abc"
 
-
-@app.route("/api/login", methods={"POST"})
-def login():
-    body = request.get_json()
-    if "email" in body and "password" in body:
-        return json_response({"token": token})
-    return json_response({"error": "invalid credentials"}, 401)
-
-
-@app.route("/api/foo", methods=["GET"])
-def foo():
-    return json_response({"hello": "world"})
-
-
-@app.route("/api/bar", methods=["GET"])
-def bar():
-    authorization = request.headers.get("authorization", None)
-    if authorization != token:
-        return json_response({'error': 'no authorization token provied'}, 401)
-    return json_response({"Wow": "you are authenticated"})
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return json_response({"error": "not found"}, 404)
+@app.route("/", methods={"GET"})
+def index():
+    return json_response({"message": "these are examples of some environment variables. (normally you would not print these!)",
+                          "variables": {"FLASK_ENV": os.getenv("FLASK_ENV"),
+                                        "MY_SECRET_VARIABLE": os.getenv("MY_SECRET_VARIABLE"),
+                                        "SMTP_PASSWORD": os.getenv("SMTP_PASSWORD")
+                                        }})
 
 
 def json_response(payload, status=200):
